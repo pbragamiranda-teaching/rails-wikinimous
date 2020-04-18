6607f448503575934cee588d8eef46f651982120
PedroMiranda <pbragamiranda@gmail.com>
2020-04-18 16:40:45 -0300

    pushing wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+100.times do 
+	Article.create(
+		title: Faker::Quote.famous_last_words,
+		content: Faker::Movies::VForVendetta.quote)
+end
